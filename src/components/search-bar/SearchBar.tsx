06797706c4e56ffce8ea174f05fc8f0a49f9173a
billy-yuan <billy.yuan1@gmail.com>
2022-02-03 18:58:09 -0500

    added search bar

--- src/components/search-bar/SearchBar.tsx
+++ src/components/search-bar/SearchBar.tsx
@@ -1,21 +1,45 @@
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { fetchData } from "../../utility/api";
+import { BASE_URL, SEARCH_ENDPOINT } from "../../utility/endpoints";
 
 function SearchBar() {
   const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    navigate("/results");
+    setIsLoading(true);
+    const queryParameters = `?q=${encodeURIComponent(searchQuery)}`;
+    const url = `${BASE_URL}${SEARCH_ENDPOINT}${queryParameters}`;
+    const response = await fetchData(url);
+
+    // const response = {
+    //   status: 200,
+    //   body: [{ test: "works" }],
+    // };
+
+    if (response.status === 200) {
+      setIsLoading(false);
+      navigate("/results", { state: response.body });
+    } else {
+      setIsLoading(false);
+      setIsError(true);
+    }
   };
 
   return (
     <>
+      {isError && <p>There was an error</p>}
+      {isLoading && <p>Loading...</p>}
       <form method="/get" onSubmit={(e) => handleSubmit(e)}>
         <label>
           Search for a restaurant
-          <input type="text" />
+          <input type="text" onChange={(e) => setSearchQuery(e.target.value)} />
         </label>
-        <button type="submit" value="Submit">
+        <button type="submit" value="Submit" disabled={searchQuery === ""}>
           Search
         </button>
       </form>
