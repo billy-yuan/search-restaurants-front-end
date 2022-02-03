05bd09e46a1333f5d3687be669a8bcc6897a717c
billy-yuan <billy.yuan1@gmail.com>
2022-02-03 12:13:15 -0500

    add search bar + home

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/home";
+import Results from "./components/results/Results";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/results" element={<Results />} />
       </Routes>
     </div>
   );
--- src/components/search-bar/SearchBar.tsx
+++ src/components/search-bar/SearchBar.tsx
@@ -1,7 +1,24 @@
+import { Link, Navigate, useNavigate } from "react-router-dom";
+
 function SearchBar() {
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    navigate("/results");
+  };
+
   return (
     <>
-      <input type="text" />
+      <form method="/get" onSubmit={(e) => handleSubmit(e)}>
+        <label>
+          Search for a restaurant
+          <input type="text" />
+        </label>
+        <button type="submit" value="Submit">
+          Search
+        </button>
+      </form>
     </>
   );
 }
