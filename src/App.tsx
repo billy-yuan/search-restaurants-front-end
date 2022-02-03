3958d2925c591704040bbd716ee566c20258ba18
Billy Yuan <billy.yuan1@gmail.com>
2022-02-03 12:42:42 -0500

    Add router (#1)
    * add base components
    
    * add search bar + home

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./components/home";
+import Results from "./components/results/Results";
 
 function App() {
-  return <div className="App">Home</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/results" element={<Results />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/home/Home.tsx
@@ -0,0 +1,11 @@
+import SearchBar from "../search-bar";
+
+function Home() {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+export default Home;
