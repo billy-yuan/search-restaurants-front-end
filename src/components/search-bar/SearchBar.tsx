341a6c57e352c0817e5dc413271ed1695e1ac394
billy-yuan <billy.yuan1@gmail.com>
2022-02-04 08:59:22 -0500

    change import path

--- src/components/search-bar/SearchBar.tsx
+++ src/components/search-bar/SearchBar.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { fetchData } from "../../utility/api";
-import { BASE_URL, SEARCH_ENDPOINT } from "../../utility/endpoints";
+import { BASE_URL, SEARCH_ENDPOINT } from "../../utility/api/endpoints";
 
 function SearchBar() {
   const navigate = useNavigate();
--- src/utility/endpoints.ts
+++
@@ -1,4 +0,0 @@
-const BASE_URL = "http://localhost:8000";
-const SEARCH_ENDPOINT = "/search";
-
-export { BASE_URL, SEARCH_ENDPOINT };
