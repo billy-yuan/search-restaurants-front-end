f47c9c219485a2dce7380828f8e15e3353b903cd
billy-yuan <billy.yuan1@gmail.com>
2022-02-04 09:20:24 -0500

    add initial card

--- src/components/restaurant-card/RestaurantCard.tsx
+++ src/components/restaurant-card/RestaurantCard.tsx
@@ -1,7 +1,24 @@
+function Categories({ categories }: { categories: string[] }) {
+  if (categories === []) {
+    return <></>;
+  }
+  return <>{categories.join(" ")}</>;
+}
+
 function RestaurantCard({ restaurant }: any) {
   console.log(restaurant);
 
-  return <div>Name: {restaurant.name}</div>;
+  restaurant.categories = ["Peruvian", "Bacon"];
+  return (
+    <div style={{ margin: "20px" }}>
+      <div>Name: {restaurant.name}</div>
+      <div>Address: {restaurant.address}</div>
+      <div>
+        <Categories categories={restaurant.categories} />
+      </div>
+      <div>{restaurant.price}</div>
+    </div>
+  );
 }
 
 export default RestaurantCard;
