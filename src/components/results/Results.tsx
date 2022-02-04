d6905b96447ae65f795046de76ed5d9a89f370bd
billy-yuan <billy.yuan1@gmail.com>
2022-02-03 19:59:10 -0500

    connect json payload to search results page

--- src/components/results/Results.tsx
+++ src/components/results/Results.tsx
@@ -1,5 +1,33 @@
+import { useLocation } from "react-router-dom";
+import RestaurantCard from "../restaurant-card";
+
+function useGetState(): any[] {
+  let { state } = useLocation();
+
+  try {
+    if (
+      state !== null &&
+      state !== undefined &&
+      typeof state === "object" &&
+      state.constructor === [].constructor
+    ) {
+      return state;
+    }
+  } catch {
+    throw Error("Not an array of objects");
+  }
+}
+
 function Results() {
-  return <div>Results</div>;
+  const data = useGetState();
+  console.log(data);
+  return (
+    <div>
+      {data.map((value) => (
+        <RestaurantCard restaurant={value} />
+      ))}
+    </div>
+  );
 }
 
 export default Results;
