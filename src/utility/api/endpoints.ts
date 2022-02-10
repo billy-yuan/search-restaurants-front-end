1313e3a1c4b1b915b7abf8627beb2e9b252f9c9a
billy-yuan <billy.yuan1@gmail.com>
2022-02-10 13:05:03 -0500

    update env

--- src/utility/api/endpoints.ts
+++ src/utility/api/endpoints.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "https://search-eater-api.herokuapp.com";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const SEARCH_ENDPOINT = "/search";
 
 export { BASE_URL, SEARCH_ENDPOINT };
