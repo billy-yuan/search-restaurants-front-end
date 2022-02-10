4d03dbbb0738d08be3c73651c1db7980708ff871
billy-yuan <billy.yuan1@gmail.com>
2022-02-10 08:12:58 -0500

    reset state when going to home page

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,6 +1,15 @@
+import { useContext, useEffect } from "react";
+import { stateContext } from "../../utility/context/appState";
 import SearchBar from "../search-bar";
 import "./style.css";
 function Home() {
+  const { setDataState } = useContext(stateContext);
+
+  // Reset data state when home page loads
+  useEffect(() => {
+    setDataState({ query: "", data: [] });
+  }, []);
+
   return (
     <div className="home-search-container">
       <SearchBar />
