f08863c1c7e09aea1bfd55da073eea7da3973431
billy-yuan <billy.yuan1@gmail.com>
2022-02-23 10:43:00 -0500

    scroll to top on page load

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import ReactMarkdown from "react-markdown";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../logo";
 import { aboutText } from "./aboutText";
@@ -5,11 +7,19 @@ import "./style.css";
 
 export function About() {
   const navigate = useNavigate();
+
+  // Scroll to top of page on page load
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="about-container">
       <div className="about-logo">
         <Logo onClick={() => navigate("/")} />
-        <div className="about-text">{aboutText}</div>
+        <div className="about-text">
+          <ReactMarkdown>{aboutText}</ReactMarkdown>
+        </div>
       </div>
     </div>
   );
